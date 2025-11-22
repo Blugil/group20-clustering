da5c95a423d69149955dd67738be8b69a5680f1c
Blugil <dagnello@asu.edu>
2025-11-21 22:26:33 -0700

    finished the json pipeline for all combinations of embeddings with clusters with vis algorithms, works dynamically on any number of each

--- src/vis/vis.py
+++ src/vis/vis.py
@@ -1,7 +1,8 @@
-from pathlib import Path
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import pacmap
+from pathlib import Path
 from sklearn.manifold import TSNE
 
 
@@ -49,11 +50,15 @@ def tsne():
         exit()
 
     # creates a tsne reduction for each embedding in the folder
-    cluster_files = [f.name for f in (EMBED_DIR).iterdir() if f.is_file() and f.name.split("_")[0] == "embed"]
-    for file in cluster_files:
+    embed_files = [f.name for f in (EMBED_DIR).iterdir() if f.is_file() and f.name.split("_")[0] == "embed"]
+    for file in embed_files:
         # file names are in the structure of:
         # cluster type, embedding type, hyperparameter
         embedding_algo = file.split("_")[1].split(".")[0]
+
+        if Path.exists(VIS_PATH / f"tsne_{embedding_algo}.npy"):
+            print(f"tsne_{embedding_algo}.npy already exists, no need to recompute")
+            continue
 
         embeddings = np.load(EMBED_DIR / file)
         tsne = TSNE(
@@ -72,12 +77,53 @@ def tsne():
         print("Saved t-SNE output to tsne_output.npy")
 
 
-    # test loads a saved vis file for graphing
-    clusters = np.load(CLUSTER_DIR /"spec_minilm_n20.npy")
-    embeddings = np.load(VIS_PATH / "tsne_minilm.npy")
-    
-    plot(embeddings_2d, clusters)
+
+def pmap():
+    #TODO: temporary imports, real vis work will involve creating a tsne or pacmap output for every cluster combo 
+    ROOT = Path(__file__).resolve().parents[2]
+    DATA_DIR = ROOT / "data"
+    EMBED_DIR = DATA_DIR / "embed"
+    CLUSTER_DIR = DATA_DIR / "cluster"
+    VIS_PATH = DATA_DIR / "vis"
+
+    # relevant later
+    # ids_path = os.path.join("../../embed/", "ids_minilm.npy")
+
+    if not os.path.isdir(VIS_PATH):
+        print("create the vis folder\naborting..")
+        exit()
+
+    # creates a tsne reduction for each embedding in the folder
+    embed_files = [f.name for f in (EMBED_DIR).iterdir() if f.is_file() and f.name.split("_")[0] == "embed"]
+    for file in embed_files:
+        # file names are in the structure of:
+        # cluster type, embedding type, hyperparameter
+        embedding_algo = file.split("_")[1].split(".")[0]
+
+        if Path.exists(VIS_PATH / f"pmap_{embedding_algo}.npy"):
+            print(f"pmap_{embedding_algo}.npy already exists, no need to recompute")
+            continue
+
+        embeddings = np.load(EMBED_DIR / file)
+
+    # testing pacmap visualization
+        pmap = pacmap.PaCMAP(
+            n_components=2, 
+            n_neighbors=10, 
+            MN_ratio=0.5, 
+            FP_ratio=2.0,
+            verbose=True,
+            num_iters=450
+        ) 
+
+        embeddings_2d = pmap.fit_transform(embeddings)
+
+        # Save the 2D t-SNE output to a new .npy file
+        np.save(VIS_PATH / f"pmap_{embedding_algo}.npy", embeddings_2d)
+        print(f"Saved PacMAP output to pmap_{embedding_algo}.npy")
 
 
 if __name__ == "__main__":
+    # compute the tsne and pmap reductions
     tsne()
+    pmap()
